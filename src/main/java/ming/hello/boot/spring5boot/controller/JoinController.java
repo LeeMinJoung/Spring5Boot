274c78540acce60b4eac10e324f403ed4a0c461d
LeeMinJoung <cocoa8192@kbu.ac.kr>
2023-07-07 15:05:45 +0900

    회원가입 : JoinController joinmeok 추가

--- src/main/java/ming/hello/boot/spring5boot/controller/JoinController.java
+++ src/main/java/ming/hello/boot/spring5boot/controller/JoinController.java
@@ -1,8 +1,11 @@
 package ming.hello.boot.spring5boot.controller;
 
 import ming.hello.boot.spring5boot.model.Checkme;
+import ming.hello.boot.spring5boot.model.Member;
+import ming.hello.boot.spring5boot.service.MemberService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +17,8 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/join")
 public class JoinController {
+
+    @Autowired MemberService msrv;
 
     Logger logger = LogManager.getLogger(JoinController.class);
 
@@ -51,6 +56,17 @@ public class JoinController {
         logger.info("join/joinme 호출!!");
 
         return "join/joinme";
+    }
+
+    @PostMapping ("/joinme")
+    public String joinmeok(Member m) {
+        logger.info("join/joinmeok 호출!!");
+        String viewPage = "redirect:/join/fail";
+
+        if (msrv.saveMember(m))
+            viewPage = "redirect:/join/joinok";
+
+        return viewPage;
     }
 
     @GetMapping("/joinok")

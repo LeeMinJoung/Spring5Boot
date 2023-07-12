84b4e1098efb5a311d5cab303b51b3d496befb70
LeeMinJoung <cocoa8192@kbu.ac.kr>
2023-07-12 10:57:52 +0900

    Member : login 추가

--- src/main/java/ming/hello/boot/spring5boot/controller/JoinController.java
+++ src/main/java/ming/hello/boot/spring5boot/controller/JoinController.java
@@ -36,7 +36,7 @@ public class JoinController {
         return "join/checkme";
     }
 
-    @PostMapping ("/checkme")
+    @PostMapping("/checkme")
     public String checkmeok(Checkme checkme, HttpSession sess) {
         logger.info("join/checkmeok 호출!!");
         // checkme에 보낸 개인정보를 세션에 저장하고
@@ -52,13 +52,17 @@ public class JoinController {
     }
 
     @GetMapping("/joinme")
-    public String joinme() {
+    public String joinme(HttpSession sess) {
         logger.info("join/joinme 호출!!");
+
+        // 세션변수가 없다면 agree로 이동
+        if (sess.getAttribute("checkme") == null)
+            return "redirect:/join/agree";
 
         return "join/joinme";
     }
 
-    @PostMapping ("/joinme")
+    @PostMapping("/joinme")
     public String joinmeok(Member m) {
         logger.info("join/joinmeok 호출!!");
         String viewPage = "redirect:/join/fail";
@@ -69,6 +73,7 @@ public class JoinController {
         return viewPage;
     }
 
+
     @GetMapping("/joinok")
     public String joinok() {
         logger.info("join/joinok 호출!!");
@@ -76,37 +81,58 @@ public class JoinController {
         return "join/joinok";
     }
 
+
     // 우편번호 검색
     // /join/zipcode?dong=동이름
+    // /join/zipcode/동이름
     // 검색결과는 뷰페이지 없이 바로 응답으로 출력 : RESTful 방식
     // 서블릿에서 제공하는 HttpServletResponse 객체를 이용하면
     // 스프링의 뷰리졸버 없이 바로 응답으로 출력할 수 있음
     // 단, 응답유형은 JSON 형식으로 함
-
     @GetMapping("/zipcode")
     @ResponseBody
     public void findzip(String dong, HttpServletResponse res) throws IOException {
 
         // 우편번호 조회결과를 JSON 형식으로 보냄
-        // 따라서, 응답유형을 JSON 형식으로 지정
+        // 따라서, 응답유형을 JSON형식으로 지정
         res.setContentType("application/json; charset=utf-8");
-        // 검색된 결과를 뷰 없이 바로 응답(response)으로 출력
-        res.getWriter().print(msrv.findzip(dong));
+        // 검색된 결과를 뷰 없이 바로 응답response으로 출력
+        res.getWriter().print( msrv.findzip(dong) );
 
     }
 
     // 아이디 중복검사
     // /join/checkuid?uid=아이디
     // /join/checkuid/아이디
-
-
     @GetMapping("/checkuid/{uid}")
     @ResponseBody
-    public void checkuid(@PathVariable  String uid, HttpServletResponse res) throws IOException {
+    public void checkuid(@PathVariable String uid, HttpServletResponse res) throws IOException {
         res.setContentType("application/json; charset=utf-8");
 
-        res.getWriter().print(msrv.checkuid(uid));
+        res.getWriter().print( msrv.checkuid(uid) );
 
+    }
+
+    // 로그인 처리
+    @PostMapping("/login")
+    public String login(Member m, HttpSession sess) {
+        logger.info("login 호출!!");
+        String returnPage = "redirect:/loginfail";
+
+        m = msrv.readOneMember(m);
+        if (m != null) {
+            sess.setAttribute("member", m);
+            returnPage = "redirect:/";
+        }
+
+        return returnPage;
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession sess) {
+        sess.invalidate();
+
+        return "redirect:/";
     }
 
 }

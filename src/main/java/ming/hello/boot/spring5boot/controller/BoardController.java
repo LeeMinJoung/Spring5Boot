6f4e415d75ee3f7a90e500f02c543396313f8d6c
LeeMinJoung <cocoa8192@kbu.ac.kr>
2023-07-12 11:24:50 +0900

    게시판 : BoardController list 구현

--- src/main/java/ming/hello/boot/spring5boot/controller/BoardController.java
+++ src/main/java/ming/hello/boot/spring5boot/controller/BoardController.java
@@ -1,21 +1,28 @@
 package ming.hello.boot.spring5boot.controller;
 
+import lombok.RequiredArgsConstructor;
+import ming.hello.boot.spring5boot.service.BoardService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 @RequestMapping("/board")
+@RequiredArgsConstructor
 public class BoardController {
+
+    final BoardService bsrv;
 
     Logger logger = LogManager.getLogger(BoardController.class);
 
     @GetMapping("/list")
-    public String list() {
+    public String list(Model m, Integer cpg) {
         logger.info("board/list 호출!!");
+
+        m.addAttribute("bds", bsrv.readBoard(cpg));
 
         return "board/list";
     }

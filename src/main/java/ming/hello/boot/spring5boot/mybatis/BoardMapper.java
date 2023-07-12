c59d346f767c95800c9f6f29096de923e213b3ae
LeeMinJoung <cocoa8192@kbu.ac.kr>
2023-07-12 10:35:16 +0900

    게시판 : board mapper 생성

---
+++ src/main/java/ming/hello/boot/spring5boot/mybatis/BoardMapper.java
@@ -0,0 +1,16 @@
+package ming.hello.boot.spring5boot.mybatis;
+
+import ming.hello.boot.spring5boot.model.Board;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+    int insertBoard(Board b);
+
+    List<Board> selectBoard();
+
+    Board selectOneBoard(String bno);
+
+}

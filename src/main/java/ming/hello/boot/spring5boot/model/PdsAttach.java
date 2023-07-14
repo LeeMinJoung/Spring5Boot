a62e8f4f0564c46059100ab0a9d6d11961888c86
LeeMinJoung <cocoa8192@kbu.ac.kr>
2023-07-14 16:05:17 +0900

    자료실 - PdsAttach 클래스 추가

---
+++ src/main/java/ming/hello/boot/spring5boot/model/PdsAttach.java
@@ -0,0 +1,19 @@
+package ming.hello.boot.spring5boot.model;
+
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class PdsAttach {
+    private String pano;
+    private String pno;
+    private String fname;
+    private String ftype;
+    private String fsize;
+    private String fdown;
+}

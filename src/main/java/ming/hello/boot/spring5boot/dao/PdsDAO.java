8b9fbf04ed948543bb7459071a8af24f790ad7b6
LeeMinJoung <cocoa8192@kbu.ac.kr>
2023-07-14 16:11:43 +0900

    자료실 - PdsDAO insertPds 구현

---
+++ src/main/java/ming/hello/boot/spring5boot/dao/PdsDAO.java
@@ -0,0 +1,8 @@
+package ming.hello.boot.spring5boot.dao;
+
+import ming.hello.boot.spring5boot.model.Pds;
+
+public interface PdsDAO {
+
+    int insertPds(Pds p);
+}
---
+++ src/main/java/ming/hello/boot/spring5boot/dao/PdsDAOImpl.java
@@ -0,0 +1,23 @@
+package ming.hello.boot.spring5boot.dao;
+
+import ming.hello.boot.spring5boot.model.Pds;
+import ming.hello.boot.spring5boot.mybatis.PdsMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("pdao")
+public class PdsDAOImpl implements PdsDAO{
+    @Autowired
+    private PdsMapper pdsMapper;
+
+    @Override
+    public int insertPds(Pds p) {
+        int cnt = pdsMapper.insertPds(p);
+
+        if(cnt > 0)
+            cnt = pdsMapper.lastPdsPno();
+
+        return cnt;
+    }
+
+}

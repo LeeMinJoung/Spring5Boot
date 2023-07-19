063dd32e9eb349d8ada7cbe341aacf03c6976a83
LeeMinJoung <cocoa8192@kbu.ac.kr>
2023-07-19 14:03:16 +0900

    댓글 - cmtwrite, replywrite 구현, view에서 댓글/대댓글 출력

--- src/main/java/ming/hello/boot/spring5boot/controller/PdsController.java
+++ src/main/java/ming/hello/boot/spring5boot/controller/PdsController.java
@@ -2,9 +2,14 @@ package ming.hello.boot.spring5boot.controller;
 
 import lombok.RequiredArgsConstructor;
 import ming.hello.boot.spring5boot.model.Pds;
+import ming.hello.boot.spring5boot.model.PdsComment;
 import ming.hello.boot.spring5boot.service.PdsService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +17,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/pds")
@@ -65,9 +72,56 @@ public class PdsController {
     public String view(Model m, @PathVariable String pno) {
         logger.info("pds/view 호출!!");
 
-        m.addAttribute("p", psrv.readOnePds(pno));
+        m.addAttribute("p", psrv.readOnePds(pno));          // 본문글
+        m.addAttribute("pcs", psrv.readPdsComment(pno));     // 댓글, 대댓글
 
         return "pds/view";
+    }
+
+    @GetMapping("/down/{pno}")
+    public ResponseEntity<Resource> down(@PathVariable String pno) {
+        logger.info("pds/down 호출!!");
+
+        // 업로드한 파일에 대한 파일명 알아냄
+        String fname = psrv.readOnePdsAttach(pno);
+
+        // 알아낸 파일명을 이용해서 헤더와 리소스 객체 생성
+        Map<String, Object> objs = psrv.getHeaderResource(fname);
+
+
+        return ResponseEntity.ok()
+                .headers((HttpHeaders)objs.get("header"))
+                .body((UrlResource)objs.get("resource"));
+    }
+
+    @PostMapping("/cmt/write")
+    public String cmtwriteok(PdsComment pc) {
+        logger.info("pds/cmt/writeok 호출!!");
+
+        String returnPage = "redirect:/pds/fail";
+
+        // 작성한 댓글을 테이블에 저장
+        if (psrv.newPdsComment(pc)) {
+            // 작성한 댓글을 확인하기 위해 바로 본문 출력
+            returnPage = "redirect:/pds/view/" + pc.getPno();
+        }
+
+        return returnPage;
+    }
+
+    @PostMapping("/reply/write")
+    public String rpywriteok(PdsComment pc) {
+        logger.info("pds/reply/writeok 호출!!");
+
+        String returnPage = "redirect:/pds/fail";
+
+        // 작성한 댓글을 테이블에 저장
+        if (psrv.newPdsReply(pc)) {
+            // 작성한 대댓글을 확인하기 위해 바로 본문 출력
+            returnPage = "redirect:/pds/view/" + pc.getPno();
+        }
+
+        return returnPage;
     }
 
 }

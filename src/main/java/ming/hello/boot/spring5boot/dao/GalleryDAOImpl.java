c57d9e2e496a38a1095a33da9f63e30be95f7d8e
LeeMinJoung <cocoa8192@kbu.ac.kr>
2023-07-20 11:19:05 +0900

    갤러리 : GalleryDAO 추가

---
+++ src/main/java/ming/hello/boot/spring5boot/dao/GalleryDAOImpl.java
@@ -0,0 +1,21 @@
+package ming.hello.boot.spring5boot.dao;
+
+import ming.hello.boot.spring5boot.model.Gallery;
+import ming.hello.boot.spring5boot.mybatis.GalleryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("gdao")
+public class GalleryDAOImpl implements GalleryDAO {
+
+    @Autowired
+    private GalleryMapper galleryMapper;
+
+    @Override
+    public List<Gallery> selectGallery(int stnum) {
+
+        return galleryMapper.selectGallery(stnum);
+    }
+}

d938b233daea4921d8b5ff215b1642a62eb0631e
LeeMinJoung <cocoa8192@kbu.ac.kr>
2023-07-20 11:17:45 +0900

    갤러리 : Gallery / GalAttach 클래스 추가

---
+++ src/main/java/ming/hello/boot/spring5boot/model/GalAttach.java
@@ -0,0 +1,19 @@
+package ming.hello.boot.spring5boot.model;
+
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class GalAttach {
+
+    private String gano;
+    private String gno;
+    private String fname;
+    private String fsize;
+
+}
